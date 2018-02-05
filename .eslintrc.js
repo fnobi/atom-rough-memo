cd2f15576eacf6bbabadf844deb766007ce3dbf2
nobii <fujisawa-shin@kayac.com>
2018-02-06 01:27:15 +0900

    eslintなど

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": [
+            "error",
+            "always-multiline"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+        "key-spacing": [
+            "error"
+        ]
+    },
+    "globals": {
+        ga: true,
+        firebase: true,
+        process: true,
+        atom: true
+    }
+};
